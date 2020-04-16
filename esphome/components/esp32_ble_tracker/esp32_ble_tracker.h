a10c203a5fd22ba7430bcac43ab26c15633d211f
Alexander Pohl <alex@ahpohl.com>
2020-04-16 16:52:49 +0200

    major code reorganisation

--- esphome/components/esp32_ble_tracker/esp32_ble_tracker.h
+++ esphome/components/esp32_ble_tracker/esp32_ble_tracker.h
@@ -89,6 +89,7 @@ class ESPBTDevice {
   const std::vector<ServiceData> &get_manufacturer_datas() const { return manufacturer_datas_; }
 
   const std::vector<ServiceData> &get_service_datas() const { return service_datas_; }
+  const ServiceData &get_service_data() const { return service_data_; }
 
   optional<ESPBLEiBeacon> get_ibeacon() const {
     for (auto &it : this->manufacturer_datas_) {
@@ -114,6 +115,7 @@ class ESPBTDevice {
   std::vector<ESPBTUUID> service_uuids_;
   std::vector<ServiceData> manufacturer_datas_{};
   std::vector<ServiceData> service_datas_{};
+  ServiceData service_data_;
 };
 
 class ESP32BLETracker;
--- esphome/components/xiaomi_ble/xiaomi_ble.cpp
+++ esphome/components/xiaomi_ble/xiaomi_ble.cpp
@@ -15,13 +15,29 @@ namespace xiaomi_ble {
 
 static const char *TAG = "xiaomi_ble";
 
-bool parse_xiaomi_data_byte(uint8_t data_type, const uint8_t *data, uint8_t data_length, XiaomiParseResult &result) {
-  /*
-    if ((data_length < 1) || (data_length > 4)) {
-      ESP_LOGD(TAG, "parse_xiaomi_data_byte(): payload has unknown size (%d)!", data_length);
-      return false;
-    }
-  */
+bool parse_xiaomi_message(std::vector<uint8_t> &raw, XiaomiParseResult &result) {
+
+  // Data point specs
+  // Byte 0: type
+  // Byte 1: fixed 0x10
+  // Byte 2: length
+  // Byte 3..3+len-1: data point value
+
+  uint8_t *raw_data = raw.data() + result.raw_offset;
+
+  // ESP_LOGD(TAG, "parse_xiaomi_data_byte(): type %02x, data %s", raw_data[0],
+  //         hexencode(raw_data + 3, raw_data[2]).c_str());
+
+  uint8_t data_type = raw_data[0];
+  uint8_t data_length = raw_data[2]);
+  uint8_t data[7] = {0};
+
+  if ((data_length < 1) || (data_length > 4)) {
+     ESP_LOGVV(TAG, "parse_xiaomi_data_byte(): payload has wrong size (%d)!", data_length);
+     return false;
+  }
+
+  memcpy(data, raw_data, data_length);
   switch (data_type) {
     case 0x0D: {  // temperature+humidity, 4 bytes, 16-bit signed integer (LE) each, 0.1 °C, 0.1 %
       if (data_length != 4)
@@ -75,23 +91,28 @@ bool parse_xiaomi_data_byte(uint8_t data_type, const uint8_t *data, uint8_t data
     default:
       return false;
   }
+
+  return false;
 }
 
-bool parse_xiaomi_service_data(XiaomiParseResult &result, const esp32_ble_tracker::ServiceData &service_data) {
+optional<XiaomiParseResult> parse_xiaomi_header(const esp32_ble_tracker::ESPBTDevice &device) {
+  XiaomiParseResult result;
+
+  esp32_ble_tracker::ServiceData service_data = device.get_service_data();
+  ESP_LOGVV(TAG, "parse_xiaomi_header(): %s", hexencode(service_data.data.data(),
+                                                                   service_data.data.size()).c_str());
+
   if (!service_data.uuid.contains(0x95, 0xFE)) {
-    ESP_LOGVV(TAG, "parse_xiaomi_service_data(): no service data UUID magic bytes");
-    return false;
+    ESP_LOGVV(TAG, "parse_xiaomi_header(): no service data UUID magic bytes");
+    return {};
   }
 
   auto raw = service_data.data;
 
   if (raw.size() < 14) {
-    ESP_LOGVV(TAG, "parse_xiaomi_service_data(): service data too short (%d)!", raw.size());
-    return false;
+    ESP_LOGVV(TAG, "parse_xiaomi_header(): service data too short (%d)!", raw.size());
+    return {};
   }
-
-  // ESP_LOGD(TAG, "parse_xiaomi_service_data(): data packet received.");
-  // ESP_LOGD(TAG, "Packet : %s", hexencode(raw.data(), raw.size()).c_str());
 
   bool is_lywsdcgq = (raw[1] & 0x20) == 0x20 && raw[2] == 0xAA && raw[3] == 0x01;
   bool is_hhccjcy01 = (raw[1] & 0x20) == 0x20 && raw[2] == 0x98 && raw[3] == 0x00;
@@ -100,9 +121,15 @@ bool parse_xiaomi_service_data(XiaomiParseResult &result, const esp32_ble_tracke
   bool is_lywsd03mmc = (raw[1] & 0x58) == 0x58 && raw[2] == 0x5b && raw[3] == 0x05;
 
   if (!is_lywsdcgq && !is_hhccjcy01 && !is_lywsd02 && !is_cgg1 && !is_lywsd03mmc) {
-    ESP_LOGVV(TAG, "parse_xiaomi_service_data(): no magic bytes");
-    return false;
+    ESP_LOGVV(TAG, "parse_xiaomi_header(): no magic bytes");
+    return {};
   }
+
+  result.raw_offset = is_lywsdcgq || is_cgg1 || is_lywsd03mmc ? 11 : 12;
+
+  result.has_data = (raw[0] & 0x40) ? true : false;
+  result.has_capability = (raw[0] & 0x20) ? true : false;
+  result.has_encryption = (raw[0] & 0x08) ? true : false;
 
   result.type = XiaomiParseResult::TYPE_HHCCJCY01;
   if (is_lywsdcgq) {
@@ -115,148 +142,47 @@ bool parse_xiaomi_service_data(XiaomiParseResult &result, const esp32_ble_tracke
     result.type = XiaomiParseResult::TYPE_LYWSD03MMC;
   }
 
-  if (is_lywsd03mmc && (raw[0] & 0x08)) {
-    ESP_LOGVV(TAG, "parse_xiaomi_service_data(): encrypted packet received.");
+  return result;
+}
+
+bool XiaomiListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
+  auto res = parse_xiaomi_header(device);
+  if (!res.has_value()) {
     return false;
   }
 
-  uint8_t raw_offset = is_lywsdcgq || is_cgg1 || is_lywsd03mmc ? 11 : 12;
-
-  // Data point specs
-  // Byte 0: type
-  // Byte 1: fixed 0x10
-  // Byte 2: length
-  // Byte 3..3+len-1: data point value
-
-  uint8_t *raw_data = raw.data() + raw_offset;
-  // ESP_LOGD(TAG, "parse_xiaomi_data_byte(): type %02x, data %s", raw_data[0],
-  //         hexencode(raw_data + 3, raw_data[2]).c_str());
-
-  if (parse_xiaomi_data_byte(raw_data[0], raw_data + 3, raw_data[2], result)) {
-    return true;
-  }
-
-  // the following code is not necessary as the payload format is fixed
-  /*
-  while (true) {
-    if (data_length < 4)
-      // at least 4 bytes required
-      // type, fixed 0x10, length, 1 byte value
-      break;
-
-    const uint8_t datapoint_type = raw_data[data_offset + 0];
-    const uint8_t datapoint_length = raw_data[data_offset + 2];
-
-    if (data_length < 3 + datapoint_length)
-      // 3 fixed bytes plus value length
-      break;
-
-    const uint8_t *datapoint_data = &raw_data[data_offset + 3];
-
-    if (parse_xiaomi_data_byte(datapoint_type, datapoint_data, datapoint_length, result)) {
-      success = true;
-      break;
-    }
-
-    data_length -= data_offset + 3 + datapoint_length;
-    data_offset += 3 + datapoint_length;
-  }
-  */
-
-  return false;
-}
-
-optional<XiaomiParseResult> parse_xiaomi(const esp32_ble_tracker::ESPBTDevice &device) {
-  XiaomiParseResult result;
-  bool success = false;
-  for (auto &service_data : device.get_service_datas()) {
-    if (parse_xiaomi_service_data(result, service_data))
-      success = true;
-  }
-  if (!success) {
-    return {};
-  }
-
   const char *name = "HHCCJCY01";
-  if (result.type == XiaomiParseResult::TYPE_LYWSDCGQ) {
+  if (res->type == XiaomiParseResult::TYPE_LYWSDCGQ) {
     name = "LYWSDCGQ";
-  } else if (result.type == XiaomiParseResult::TYPE_LYWSD02) {
+  } else if (res->type == XiaomiParseResult::TYPE_LYWSD02) {
     name = "LYWSD02";
-  } else if (result.type == XiaomiParseResult::TYPE_CGG1) {
+  } else if (res->type == XiaomiParseResult::TYPE_CGG1) {
     name = "CGG1";
-  } else if (result.type == XiaomiParseResult::TYPE_LYWSD03MMC) {
+  } else if (res->type == XiaomiParseResult::TYPE_LYWSD03MMC) {
     name = "LYWSD03MMC";
   }
 
   ESP_LOGD(TAG, "Got Xiaomi %s (%s):", name, device.address_str().c_str());
 
-  if (result.temperature.has_value()) {
-    ESP_LOGD(TAG, "  Temperature: %.1f°C", result.temperature.value());
+  if (res->temperature.has_value()) {
+    ESP_LOGD(TAG, "  Temperature: %.1f°C", *res->temperature);
   }
-  if (result.humidity.has_value()) {
-    ESP_LOGD(TAG, "  Humidity: %.1f%%", result.humidity.value());
+  if (res->humidity.has_value()) {
+    ESP_LOGD(TAG, "  Humidity: %.1f%%", *res->humidity);
   }
-  if (result.battery_level.has_value()) {
-    ESP_LOGD(TAG, "  Battery Level: %.0f%%", result.battery_level.value());
+  if (res->battery_level.has_value()) {
+    ESP_LOGD(TAG, "  Battery Level: %.0f%%", *res->battery_level);
   }
-  if (result.conductivity.has_value()) {
-    ESP_LOGD(TAG, "  Conductivity: %.0fµS/cm", result.conductivity.value());
+  if (res->conductivity.has_value()) {
+    ESP_LOGD(TAG, "  Conductivity: %.0fµS/cm", *res->conductivity);
   }
-  if (result.illuminance.has_value()) {
-    ESP_LOGD(TAG, "  Illuminance: %.0flx", result.illuminance.value());
+  if (res->illuminance.has_value()) {
+    ESP_LOGD(TAG, "  Illuminance: %.0flx", *res->illuminance);
   }
-  if (result.moisture.has_value()) {
-    ESP_LOGD(TAG, "  Moisture: %.0f%%", result.moisture.value());
-  }
-
-  return result;
-}
-
-bool XiaomiListener::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
-  auto res = parse_xiaomi(device);
-
-  if (!res.has_value()) {
-    return false;
+  if (res->moisture.has_value()) {
+    ESP_LOGD(TAG, "  Moisture: %.0f%%", *res->moisture);
   }
 
-  // ESP_LOGD(TAG, "XiaomiListener::parse_device(): res.has_value: %s", res.has_value() ? "true" : "false");
-
-  // res.has_value() always returns false when xiaomi_ble::decrypt_xiaomi_payload()
-  // is called from XiaomiLYWSD03MMC::parse_device()
-  // hence I moved the "Got Xiaomi..." device report code to xiaomi_ble::parse_xiaomi()
-  /*
-    const char *name = "HHCCJCY01";
-    if (res->type == XiaomiParseResult::TYPE_LYWSDCGQ) {
-      name = "LYWSDCGQ";
-    } else if (res->type == XiaomiParseResult::TYPE_LYWSD02) {
-      name = "LYWSD02";
-    } else if (res->type == XiaomiParseResult::TYPE_CGG1) {
-      name = "CGG1";
-    } else if (res->type == XiaomiParseResult::TYPE_LYWSD03MMC) {
-      name = "LYWSD03MMC";
-    }
-
-    ESP_LOGD(TAG, "Got Xiaomi %s (%s):", name, device.address_str().c_str());
-
-    if (res->temperature.has_value()) {
-      ESP_LOGD(TAG, "  Temperature: %.1f°C", *res->temperature);
-    }
-    if (res->humidity.has_value()) {
-      ESP_LOGD(TAG, "  Humidity: %.1f%%", *res->humidity);
-    }
-    if (res->battery_level.has_value()) {
-      ESP_LOGD(TAG, "  Battery Level: %.0f%%", *res->battery_level);
-    }
-    if (res->conductivity.has_value()) {
-      ESP_LOGD(TAG, "  Conductivity: %.0fµS/cm", *res->conductivity);
-    }
-    if (res->illuminance.has_value()) {
-      ESP_LOGD(TAG, "  Illuminance: %.0flx", *res->illuminance);
-    }
-    if (res->moisture.has_value()) {
-      ESP_LOGD(TAG, "  Moisture: %.0f%%", *res->moisture);
-    }
-  */
 
   return true;
 }
@@ -328,12 +254,13 @@ bool decrypt_xiaomi_payload(std::vector<uint8_t> &t_raw, const uint8_t *t_bindke
   ret = mbedtls_ccm_auth_decrypt(&ctx, vector.datasize, vector.iv, vector.ivsize, vector.authdata, vector.authsize,
                                  vector.ciphertext, vector.plaintext, vector.tag, vector.tagsize);
   if (ret) {
-    // ESP_LOGD(TAG, "Name : %s", vector.name);
-    // ESP_LOGD(TAG, "Packet : %s", hexencode(t_raw.data(), t_raw.size()).c_str());
-    // ESP_LOGD(TAG, "Key : %s", hexencode(vector.key, vector.keysize).c_str());
-    // ESP_LOGD(TAG, "Iv : %s", hexencode(vector.iv, vector.ivsize).c_str());
-    // ESP_LOGD(TAG, "Cipher : %s", hexencode(vector.ciphertext, vector.datasize).c_str());
-    // ESP_LOGD(TAG, "Tag : %s", hexencode(vector.tag, vector.tagsize).c_str());
+    ESP_LOGVV(TAG, "Name : %s", vector.name);
+    ESP_LOGVV(TAG, "MAC address : %s", hexencode(mac_address, 6).c_str());
+    ESP_LOGVV(TAG, "Packet : %s", hexencode(t_raw.data(), t_raw.size()).c_str());
+    ESP_LOGVV(TAG, "Key : %s", hexencode(vector.key, vector.keysize).c_str());
+    ESP_LOGVV(TAG, "Iv : %s", hexencode(vector.iv, vector.ivsize).c_str());
+    ESP_LOGVV(TAG, "Cipher : %s", hexencode(vector.ciphertext, vector.datasize).c_str());
+    ESP_LOGVV(TAG, "Tag : %s", hexencode(vector.tag, vector.tagsize).c_str());
     ESP_LOGVV(TAG, "decrypt_xiaomi_payload(): authenticated decryption failed.");
     mbedtls_ccm_free(&ctx);
     return false;
@@ -348,8 +275,8 @@ bool decrypt_xiaomi_payload(std::vector<uint8_t> &t_raw, const uint8_t *t_bindke
   // clear encrypted flag
   t_raw[0] &= ~0x08;
 
-  // ESP_LOGD(TAG, "Plaintext : %s", hexencode(t_raw.data()+11, vector.datasize).c_str());
-  // ESP_LOGD(TAG, "MAC address : %s", hexencode(mac_address, 6).c_str());
+  ESP_LOGVV(TAG, "decrypt_xiaomi_payload(): authenticated decryption passed.");
+  ESP_LOGVV(TAG, "Plaintext : %s", hexencode(t_raw.data()+11, vector.datasize).c_str());
 
   mbedtls_ccm_free(&ctx);
   return true;
