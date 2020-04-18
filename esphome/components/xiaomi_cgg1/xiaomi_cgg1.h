b36649d6b8e5b97620e7537f8379ad7a31a21514
Alexander Pohl <alex@ahpohl.com>
2020-04-18 08:46:00 +0200

    Revert "move parse_device() from .h to .cpp"
    This reverts commit 96c186b5424d31259fa9bf0afebf8cd800b15705.

--- esphome/components/xiaomi_cgg1/xiaomi_cgg1.h
+++ esphome/components/xiaomi_cgg1/xiaomi_cgg1.h
@@ -14,7 +14,40 @@ class XiaomiCGG1 : public Component, public esp32_ble_tracker::ESPBTDeviceListen
  public:
   void set_address(uint64_t address) { address_ = address; }
 
-  bool parse_device(const esp32_ble_tracker::ESPBTDevice &device) override;
+  bool parse_device(const esp32_ble_tracker::ESPBTDevice &device) override {
+    if (device.address_uint64() != this->address_)
+      return false;
+
+    auto res = xiaomi_ble::parse_xiaomi_header(device);
+    if (res->has_capability) {
+      ESP_LOGVV(TAG, "parse_device(): service data has capability flag.");
+      return false;
+    }
+
+    if (!res.has_value()) {
+      ESP_LOGVV(TAG, "parse_device(): no service data received.");
+      return false;
+    }
+
+    esp32_ble_tracker::ServiceData service_data = device.get_service_data();
+    if (res->has_encryption) {
+      ESP_LOGVV(TAG, "parse_device(): decryption is currently not supported on this device.");
+      return false;
+    }
+
+    if (!(xiaomi_ble::parse_xiaomi_message(service_data.data, *res))) {
+      ESP_LOGVV(TAG, "parse_device(): message contains no results.");
+      return false;
+    }
+
+    if (res->temperature.has_value() && this->temperature_ != nullptr)
+      this->temperature_->publish_state(*res->temperature);
+    if (res->humidity.has_value() && this->humidity_ != nullptr)
+      this->humidity_->publish_state(*res->humidity);
+    if (res->battery_level.has_value() && this->battery_level_ != nullptr)
+      this->battery_level_->publish_state(*res->battery_level);
+    return true;
+  }
 
   void dump_config() override;
   float get_setup_priority() const override { return setup_priority::DATA; }
