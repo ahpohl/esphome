8a0bf25292476806e77ae14a840d6c0ac0ccddbc
Alexander Pohl <alex@ahpohl.com>
2020-09-23 07:31:07 +0200

    add multi value parsing to xiaomi_ble

--- esphome/components/xiaomi_ble/xiaomi_ble.h
+++ esphome/components/xiaomi_ble/xiaomi_ble.h
@@ -57,10 +57,11 @@ struct XiaomiAESVector {
   size_t ivsize;
 };
 
-bool parse_xiaomi_message(const std::vector<uint8_t> &message, XiaomiParseResult &result);
 optional<XiaomiParseResult> parse_xiaomi_header(const esp32_ble_tracker::ServiceData &service_data);
-bool decrypt_xiaomi_payload(std::vector<uint8_t> &raw, const uint8_t *bindkey, const uint64_t &address);
+bool parse_xiaomi_message(const std::vector<uint8_t> &message, XiaomiParseResult &result);
+bool parse_xiaomi_value(uint8_t value_type, const uint8_t *data, uint8_t value_length, XiaomiParseResult &result);
 bool report_xiaomi_results(const optional<XiaomiParseResult> &result, const std::string &address);
+bool decrypt_xiaomi_payload(std::vector<uint8_t> &raw, const uint8_t *bindkey, const uint64_t &address);
 
 class XiaomiListener : public esp32_ble_tracker::ESPBTDeviceListener {
  public:
